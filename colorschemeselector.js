85dae1469eea2a9d435a2889dc775dc8cff3dbb7
amisha-arora <143438373+amisha-arora@users.noreply.github.com>
2024-05-20 00:16:09 +0530

    Add files via upload

---
+++ colorschemeselector.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.button');
+    const body = document.querySelector('body');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', e => {
+            const color = e.target.id;
+            body.style.backgroundColor = color;
+        });
+    });
+});
